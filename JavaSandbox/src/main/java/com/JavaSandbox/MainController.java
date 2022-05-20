1ef7ac7df31fdf7dcc9c59793730a298c52ea9ef
JCaeta <63880187+JCaeta@users.noreply.github.com>
2022-05-20 11:24:19 -0300

    algorithms updated

--- JavaSandbox/src/main/java/com/JavaSandbox/MainController.java
+++ JavaSandbox/src/main/java/com/JavaSandbox/MainController.java
@@ -1,4 +1,8 @@
 package com.JavaSandbox;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
@@ -7,10 +11,12 @@ import org.springframework.stereotype.Controller;
 public class MainController
 {
     public MainController(){
-        algorithm0();
+        // algorithm0();
+        algorithm1();
     }
 
     public void algorithm0(){
+        // Batch bidimensional string array by field
         System.out.println("algorithm0");
         String datosRecursos [][] = {
             {"Computadora", "1", "HP", "HP-123"},
@@ -37,5 +43,31 @@ public class MainController
         for(String [] row : datosRecursos){
             System.out.println(row[0] + " | " + row[1] + " | " + row[2] + " | " + row[3]);
         }
+    }
+
+    public void algorithm1()
+    {
+        // Basics of date and time
+
+        //Create date objects
+        LocalDate date0 = LocalDate.of(2022, 2, 28);
+        LocalDate date1 = LocalDate.of(2022, Month.FEBRUARY, 28);
+
+        // Create time objects
+        LocalTime time0 = LocalTime.of(12, 30, 45);
+        LocalTime time1 = LocalTime.of(12, 30);
+
+        //Create datetime objects
+        LocalDateTime dateTime0 = LocalDateTime.of(date0, time0);
+        LocalDateTime dateTime1 = LocalDateTime.of(2022, 2, 28, 12, 30, 45);
+        LocalDateTime dateTime2 = LocalDateTime.of(2022, Month.AUGUST, 28, 12, 30);
+
+        System.out.println("date0: " + date0);
+        System.out.println("date1: " + date1);
+        System.out.println("time0: " + time0);
+        System.out.println("time1: " + time1);
+        System.out.println("dateTime0: " + dateTime0);
+        System.out.println("dateTime1: " + dateTime1);
+        System.out.println("dateTime2: " + dateTime2);
     }
 }
